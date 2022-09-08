a2ee1497a65a754590925fc7c2b127d542b96828
the Horacle <princemjames2@gmail.com>
2022-09-08 16:00:58 -0700

    Second trial

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/*
+ * main - main block
+ * return: 0
+*/
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return(0);
+puts("Programming is like building a multilingual puzzle\n");
+return(0);
 }
