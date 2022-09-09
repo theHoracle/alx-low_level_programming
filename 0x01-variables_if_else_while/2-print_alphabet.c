8658bb913dd7e12b51dd50d050a4426211ec915c
the Horacle <princemjames2@gmail.com>
2022-09-09 06:47:40 -0700

    New script

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * print all characters of the alphabet
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	letter = 'a';
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	return (0);
+}
