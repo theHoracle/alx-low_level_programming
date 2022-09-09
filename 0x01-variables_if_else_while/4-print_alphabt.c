319c634879fc4242cbdb4976e38a03ad5a547e30
the Horacle <princemjames2@gmail.com>
2022-09-09 09:05:02 -0700

    New commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * print all characters except q and e
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+	
+	ch = 'a';
+	while (ch <= 'z')
+	{
+		if (ch != 'q' || ch != 'e')
+			putchar(ch);
+		ch++;
+	}
+
+	putchar('\n');
+	return (0);
+}
