26d4b88b5a62e0a19714f4e35ef70c2836de3125
the Horacle <princemjames2@gmail.com>
2022-09-09 05:37:17 -0700

    Fixed

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,14 +14,11 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 
 if (n < 0)
-  printf("%i is negative\n", n);
+	printf("%i is negative\n", n);
+else if (n > 0)
+	printf("%i is positive\n", n);
 else
-   {
-    if (n > 0)
-      printf("%i is positive\n", n);
-    else
-    {
-    printf("%i is zero\n");
-    }
-  }
-return (0); }
+	printf("%i is zero\n");
+}
+return (0);
+}
