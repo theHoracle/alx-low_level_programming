0bf8e9e27422a5bbbab1ee71d9983aabc57bae11
the Horacle <princemjames2@gmail.com>
2022-09-22 15:16:20 -0700

    second commit

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strncat - entry point
+ * @dest: destination
+ * @src: source
+ * @n: num
+ * return: new string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int a = -1, i;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		/** lets do it boyo */
+	}
+
+	do {
+		a++;
+		dest[i] = src[a];
+		i++;
+	} while (a <= n)
+
+	dest[a + 1] = '\0';
+
+	return (dest);
+}
