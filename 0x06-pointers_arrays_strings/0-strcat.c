34817c64a41587453fb887baf8340e77e90d148b
the Horacle <princemjames2@gmail.com>
2022-09-22 13:03:52 -0700

    first file

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * *_strcat - strcat
+ * @dest: src is appended to the src str
+ * @src: str in appended to dest
+ * return: pointer to the resulting str
+ */
+char *_strcat(char *dest, char *src)
+{
+	int a = 0, i;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		/* get the length of dest */
+	}
+
+
+	do {
+		a++;
+		dest[i] = src[a];
+		i++;
+	} while (src[a] != '\0');
+
+	return (dest);
+}
