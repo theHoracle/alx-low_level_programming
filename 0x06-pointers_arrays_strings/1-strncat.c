a72cc4498ed0cbc1c400b6f55a6b6ab9ce2e6754
the Horacle <princemjames2@gmail.com>
2022-09-22 15:32:08 -0700

    update

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,20 +8,19 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int a = -1, i;
+	int i2, i;
 
 	for (i = 0; dest[i] != '\0'; i++)
 	{
 		/** lets do it boyo */
 	}
 
-	do {
-		a++;
-		dest[i] = src[a];
-		i++;
-	} while (a >= n)
+	for (i2 = 0; i2 < n && str[i2] != '\0'; i2++)
+	{
+		dest[i + i2] = src[i2];
+	}
 
-	dest[a + 1] = '\0';
+	dest[i2 + 1] = '\0';
 
 	return (dest);
 }
