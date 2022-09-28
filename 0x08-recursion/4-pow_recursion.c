22c557fad294da6d3ce0417b989819339687d604
the Horacle <princemjames2@gmail.com>
2022-09-28 08:07:36 -0700

    Update

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _pow_recursion - entry point
+ * @x: int
+ * @y: power
+ * Return: int of power
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 1)
+		return (x);
+	if (y >= 0)
+	{
+		return (x * _pow_recursion(x, y-1));
+	}
+
+	return (-1)
+}
