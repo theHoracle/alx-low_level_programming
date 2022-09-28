26076f8d085edb0d4b3d9478b0522e55e32ceee9
the Horacle <princemjames2@gmail.com>
2022-09-28 06:29:36 -0700

    update

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts_recursion - puts string recursively
+ * @str: string to be printed to stdout
+ * Return: void
+ */
+void _puts_recursion(char *str)
+{
+	if (*str = '\0')
+	{
+		_putchar('\n');
+	}
+	_putchar(*str);
+	_puts_recursion(str + 1);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+Starting a fun topic... RECURSION
