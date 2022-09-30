037b88539f99343aa7774e61502d118377f226f4
the Horacle <princemjames2@gmail.com>
2022-09-30 04:36:16 -0700

    update

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 (Always)
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	for (i = 0; argv[0][i] != '\0'; i++)
+	{
+		_putchar(argv[0][i]);
+	}
+
+	_putchar('\n');
+}
