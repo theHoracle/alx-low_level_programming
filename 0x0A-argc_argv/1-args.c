bafa8bbd9fe0f2d11c13af7310b8c09851d88f9d
the Horacle <princemjames2@gmail.com>
2022-09-30 14:37:13 -0700

    update

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/***
+ * main - print no of arguments given
+ * @argc: arg coiunt
+ * @argv: arg vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", (argc - 1));
+	return (0);
+}
