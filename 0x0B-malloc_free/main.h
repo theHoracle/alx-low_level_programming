0a077e765f33a39eddbd1b2ebd06c502d9bae756
the Horacle <princemjames2@gmail.com>
2022-10-04 13:59:05 -0700

    update

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,5 @@
+char *create_array(unsigned int size, char c);
+void free_grid(int **grid, int height);
+int **alloc_grid(int width, int height);
+char *str_concat(char *s1, char *s2);
+char *_strdup(char *str);
