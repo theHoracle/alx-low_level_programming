f081b998573f69ccd7e6bb5d7b73ff596ab1f170
the Horacle <princemjames2@gmail.com>
2022-10-11 01:39:22 -0700

    update

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct for type dog
+ * @name: char
+ * @age: float
+ * @owner: char
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
