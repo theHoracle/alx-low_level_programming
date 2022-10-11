5cd1d40e54c5ff9b09181e728956039fa4782d43
the Horacle <princemjames2@gmail.com>
2022-10-11 01:50:04 -0700

    update and fixes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,14 +6,17 @@
  * @age: float
  * @owner: char
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
-/**
-* void init_dog(struct dog *d, char *name, float age, char owner);
-* void print_dog(
-*/
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 #endif
